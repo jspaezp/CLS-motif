b7622f13c29c5feab1a58f6d4be01b14624fd27b
Wyatt Baysinger <Wyatt Baysinger>
2016-12-05 00:48:37 -0500

    unsed imports trimmed

--- 001-read_databases.py
+++ 001-read_databases.py
@@ -3,7 +3,6 @@
 # USAGE EXAMPLE
 
 from reg_phos_reader import get_kinase_group, get_substrates
-from calculate_alignment_scores import calculate_alignment_scores, cross_score
 from get_windows import get_windows
 from fasta_tools import get_relevant_db
 
@@ -11,8 +10,6 @@ from Bio import SeqIO
 from Bio.Alphabet import IUPAC
 from Bio import motifs
 
-import numpy as num
-import matplotlib.pyplot as plt
 import pandas as pd
 
 my_kinases = get_kinase_group("./regPhos/RegPhos_kinase_human.txt",
@@ -65,47 +62,4 @@ score_lists[0].head()["id"]
 
 my_data_frame = pd.DataFrame()
 my_data_frame['kinase'] = my_kinases
-my_data_frame['matches'] = score_list
-
-#plot my_scores histogram to pick out cutoff
-scores_hist=plt.hist(my_scores)
-
-
-
-# iterate over models
-headerList_ecoli=[]
-seqList_ecoli=[]
-for i in (my_substrates['substrates'].tolist()):
-    fasta_db = SeqIO.parse("./ModelOrganisms/UP000000625_83333.fasta",
-                           "fasta", IUPAC.extended_protein)
-    for record in fasta_db:
-        headerList_ecoli.append(record.id)
-        seqList_ecoli.append(str(record.seq))
-
-my_scores_ecoli=[]
-for seq in seqList_ecoli[1:5]:
-    my_scores_ecoli.append([[] if (len(pssm) == 0) else
-             calculate_alignment_scores(pssm, seq) for
-             pssm in my_pssm])
-
-headerList_scer=[]
-seqList_scer=[]
-for i in (my_substrates['substrates'].tolist()):
-    fasta_db = SeqIO.parse("./ModelOrganisms/UP000002311_559292.fasta",
-                           "fasta", IUPAC.extended_protein)
-    for record in fasta_db:
-        headerList_scer.append(record.id)
-        seqList_scer.append(str(record.seq))
-
-my_scores_scer=[]
-for seq in seqList_scer[1:5]:
-    my_scores_scer.append([[] if (len(pssm) == 0) else
-             calculate_alignment_scores(pssm, seq) for
-             pssm in my_pssm])
-
-# how many cross threshold
-
-
-# headers
-##################################################################
-
+my_data_frame['matches'] = score_lists
